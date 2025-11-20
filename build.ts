7df37590cf4afd96dd1720e3273f79b9bb4fae7b
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2025-11-20 10:47:30 +0800

    Update format

--- build.ts
+++ build.ts
@@ -14,4 +14,22 @@ PluginBuild(
     // Template metadata
     projects: FFmpeg_ProjectTempGroup,
     databases: FFmpeg_DatabaseTempGroup
+}, {
+    version: "1.0.0",
+    title: "Blender",
+    description: "pluginDes",
+    i18n: [
+        {
+            key: "en",
+            value: {
+                pluginDes: "Open-source 3D modeling tool"
+            }
+        },
+        {
+            key: "zh-TW",
+            value: {
+                pluginDes: "開源 3D 建模工具"
+            }
+        }
+    ]
 })
